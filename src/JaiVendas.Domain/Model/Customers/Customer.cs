3042246724df13e40b20d759e864d6a6142ed6e1
Jairo <jairo.m@terra.com.br>
2021-03-13 21:30:51 -0300

    Entidades relacionadas a  Customer

---
+++ src/JaiVendas.Domain/Model/Customers/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaiVendas.Domain.Model.Customers
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+
+        public string CPF { get; set; }
+
+        public string Name { get; set; }
+
+        public bool Active { get; set; }
+
+        public ICollection<CustomerPhone> Phones { get; set; }
+
+        public ICollection<CustomerAddress> Addresses { get; set; }
+    }
+}
---
+++ src/JaiVendas.Domain/Model/Customers/CustomerAddress.cs
@@ -0,0 +1,34 @@
+﻿using JaiVendas.Domain.Model.Internationalization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaiVendas.Domain.Model.Customers
+{
+    public class CustomerAddress
+    {
+        public Guid Id { get; set; }
+
+        public Guid CustomerId { get; set; }
+
+        public string Street { get; set; }
+
+        public string Number { get; set; }
+
+        public string Neighborhood { get; set; }
+
+        public string City { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public Guid CountryId { get; set; }
+        public Country Country { get; set; }
+ 
+        public Guid RegionId { get; set; }
+
+        public CountryRegion Region { get; set; }
+
+    }
+}
