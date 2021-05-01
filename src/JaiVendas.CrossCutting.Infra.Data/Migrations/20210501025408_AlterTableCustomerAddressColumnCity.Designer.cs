f9b0d78cb2255e954e56f8c9681d854bbbef4481
Jairo <jairo.m@terra.com.br>
2021-04-30 23:57:28 -0300

    Migration

--- src/JaiVendas.CrossCutting.Infra.Data/Migrations/20210501025408_AlterTableCustomerAddressColumnCity.Designer.cs
+++ src/JaiVendas.CrossCutting.Infra.Data/Migrations/20210501025408_AlterTableCustomerAddressColumnCity.Designer.cs
@@ -4,14 +4,16 @@ using JaiVendas.CrossCutting.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace JaiVendas.CrossCutting.Infra.Data.Migrations
 {
     [DbContext(typeof(JaiVendasDataContext))]
-    partial class JaiVendasDataContextModelSnapshot : ModelSnapshot
+    [Migration("20210501025408_AlterTableCustomerAddressColumnCity")]
+    partial class AlterTableCustomerAddressColumnCity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/JaiVendas.CrossCutting.Infra.Data/Migrations/20210501025408_AlterTableCustomerAddressColumnCity.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JaiVendas.CrossCutting.Infra.Data.Migrations
+{
+    public partial class AlterTableCustomerAddressColumnCity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "CustomerAddresses",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "CustomerAddresses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+        }
+    }
+}
